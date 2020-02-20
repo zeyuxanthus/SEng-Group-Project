56559a453fd8a103736a5d5a39b8537bd86225ba
NOTCANDID <61278886+NOTCANDID@users.noreply.github.com>
2020-02-20 19:08:54 +0000

    Added variables to calculations

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,15 +1,15 @@
 public class Calculations {
 
-    public int impressions = 40000; // number of impressions
-    public int clicks = 25000; // number of clicks
-    public int uniques = 20000; // number of uniques i.e. number of unique users
-    public int bounces = 15000; // number of bounces i.e. clicks on ad without interacting with website
-    public int conversions = 10000; // number of conversions i.e. clicks and acts on an ad
-    public int totalCost = 30000;
-    public int ctr = clicks / impressions; // click-through-rate
-    public int cpa = totalCost / conversions; // cost-per-acquisition
-    public int cpc = totalCost / clicks; // cost-per-click
-    public int cpm = totalCost / 1000; // cost-per-thousand impressions
-    public int bounceRate = bounces / clicks;
+    private int impressions = 40000; // number of impressions
+    private int clicks = 25000; // number of clicks
+    private int uniques = 20000; // number of uniques i.e. number of unique users
+    private int bounces = 15000; // number of bounces i.e. clicks on ad without interacting with website
+    private int conversions = 10000; // number of conversions i.e. clicks and acts on an ad
+    private int totalCost = 30000;
+    private int ctr = clicks / impressions; // click-through-rate
+    private int cpa = totalCost / conversions; // cost-per-acquisition
+    private int cpc = totalCost / clicks; // cost-per-click
+    private int cpm = totalCost / 1000; // cost-per-thousand impressions
+    private int bounceRate = bounces / clicks;
 
 }
