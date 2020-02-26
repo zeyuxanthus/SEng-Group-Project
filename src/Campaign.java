c4d169db0fb5d8ccdc896c9dea38cb989a922c95
zeyuyez <61348241+zeyuyez@users.noreply.github.com>
2020-02-26 13:16:35 +0000

    Added methods loading of csv files

--- src/Campaign.java
+++ src/Campaign.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 /**
  *  Domain of the application. (Basically backend)
