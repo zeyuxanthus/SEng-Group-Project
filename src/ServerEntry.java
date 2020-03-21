9c143be5dafc43e2d478c7e56a8522bbff9060f3
Daniel Raad <52507822+daniel-raad@users.noreply.github.com>
2020-03-21 11:58:16 +0000

    updated server entry with impression details

--- src/ServerEntry.java
+++ src/ServerEntry.java
@@ -4,24 +4,29 @@ import java.time.LocalDateTime;
  * A single entry from a Server Log
  * Contains information about user's interaction with website after clicking an add
  */
-public class ServerEntry implements Comparable<ServerEntry>{
+public class ServerEntry {
     private LocalDateTime entryDate;
     private String ID;
     private LocalDateTime exitDate;
     private int pagesViewed;
     private String conversion;
 
-  public ServerEntry(LocalDateTime entryDate, String id, LocalDateTime exitDate, int pagesViewed, String conversion) {
+
+
+    private String gender;
+    private String income;
+    private String ageGroup;
+    private String context;
+    private float impressionCost;
+
+
+
+    public ServerEntry(LocalDateTime entryDate, String id, LocalDateTime exitDate, int pagesViewed, String conversion) {
         this.entryDate = entryDate;
         this.ID = id;
         this.exitDate = exitDate;
         this.pagesViewed = pagesViewed;
         this.conversion = conversion;
-    }
-
-    @Override
-    public int compareTo(ServerEntry i) {
-        return entryDate.compareTo(i.getEntryDate());
     }
 
     enum Conversion{
@@ -33,6 +38,31 @@ public class ServerEntry implements Comparable<ServerEntry>{
         Conversion(String conversion){
             this.conversion = conversion;
         }
+    }
+
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public void setIncome(String income){
+        this.income = income;
+    }
+
+    public void setAgeGroup(String ageGroup){
+        this.ageGroup = ageGroup;
+    }
+
+    public void setContext(String context){
+        this.context = context;
+    }
+
+    public void setImpressionCost(float impCost){
+        this.impressionCost = impCost;
+    }
+
+    public String getGender(){
+        return gender;
     }
 
     public LocalDateTime getEntryDate(){
@@ -54,7 +84,7 @@ public class ServerEntry implements Comparable<ServerEntry>{
     public String getConversion(){
         return conversion;
     }
-    
+
     @Override
     public String toString() {
         return entryDate + " " + ID + " " + exitDate +
@@ -62,4 +92,3 @@ public class ServerEntry implements Comparable<ServerEntry>{
     }
 
 }
-
