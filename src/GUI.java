bd4d286b8827323e67459105c7552d0c088deeca
AJStoffer <AJStoffer@users.noreply.github.com>
2020-04-29 11:56:33 +0100

    Fixed the Histogram creator

--- src/GUI.java
+++ src/GUI.java
@@ -257,7 +257,8 @@ public class GUI extends Application {
         windowLayout.getChildren().addAll(granularityLayout, metricLayout);
         granularityLayout.setMargin(granularityLabel, new Insets(3, 0, 0, 0));
         //windowLayout.setMargin(granularityLayout, new Insets(10, 10, 5, 10));
-
+        
+        granularityLayout.setMargin(granularityLabel, new Insets(0,0,0,10));
         metricLayout.setMargin(metricLabels2, new Insets(5, 0, 0, 30));
         metricLayout.setMargin(metricLabels1, new Insets(5, 0, 0, 10));
 
@@ -296,13 +297,17 @@ public class GUI extends Application {
                filterNodes = impressionFilterOptions.getChildren();
 
 
-                for (Node n : filterNodes) {
-                    if (n instanceof TextField) {
-                        filters.add(((TextField) n).getText());
-                    } else if (n instanceof ComboBox) {
-                        filters.add((String) ((ComboBox) n).getValue());
-                    }
-                }
+               for (Node n : filterNodes) {
+               	if (n instanceof VBox) {
+               		for (Node m : ((VBox) n).getChildren()) {
+               			 if (m instanceof TextField) {
+                                filters.add(((TextField) m).getText());
+                            } else if (m instanceof ComboBox) {
+                                filters.add((String) ((ComboBox) m).getValue());
+                            }
+               		}
+               	}
+               }
 
                 LocalDateTime startDate = null;
                 LocalDateTime endDate = null;
